8e5449e6be4c50ef22ed50e6779beb86ef1aaf0a
Brian <brianemdn@gmail.com>
2018-05-21 11:38:59 -0700

    adding output for login php

--- web/website/login.php
+++ web/website/login.php
@@ -1,6 +1,7 @@
 <?php
 //make the database connection with the database environment variable
-$db_connection = pg_connect(getenv("DATABASE_URL"));
+$db_conn = pg_connect(getenv("DATABASE_URL"));
+
 
 $user_username = $_POST["username"];
 $user_password = $_POST["password"];
@@ -9,7 +10,19 @@ echo("Data received: " . $user_username . ", " . $user_password . "<br>");
 $user_username = stripslashes($user_username);
 $user_password = stripslashes($user_password);
 echo("Slashes stripped: " . $user_username . ", " . $user_password . "<br>");
-// $user_username = mysqli::escape_string($user_username);
-// $user_password = mysqli_real_escape_string($user_password);
-// echo("MySQL stuff stripped: " . $user_username . ", " . $user_password);
+
+
+$result = pg_query($db_conn, "SELECT user_id, username FROM users");
+
+if (!$result) {
+    echo ("Error querying rows from users table");
+    exit;
+}
+
+while ($row = pg_fetch_row($result)) {
+    echo ("<br><br>");
+    echo ("User Number: " . $row[0]);
+    echo ("Username: " . $row[1]);
+    echo ("<br><br>");
+}
 ?>
